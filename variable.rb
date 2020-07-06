ab90a16dbdacf7c74cb9696dadef63b2431d9199
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-06 21:31:28 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,4 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-greeting = "Hello World"
-puts "#{greeting}"
+current_president = "Barack Obama"
+puts "In 2016, the president was #{current_president}."
